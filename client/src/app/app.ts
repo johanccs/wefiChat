b51ba9425e54ba27a10cec3562c897cee6e17e10
Johan Potgieter <johan.ccs@gmail.com>
2026-01-17 17:34:54 +0200

    Initial check in

---
+++ client/src/app/app.ts
@@ -0,0 +1,15 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Home } from "./home/home";
+import { MainNav } from "./nav/main-nav/main-nav";
+import { FooterNav } from "./nav/footer-nav/footer-nav";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Home, MainNav, FooterNav],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('wefi-chat-client');
+}
---
+++ client/src/app/nav/footer-nav/footer-nav.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-nav',
+  imports: [],
+  templateUrl: './footer-nav.html',
+  styleUrl: './footer-nav.css',
+})
+export class FooterNav {
+
+}
